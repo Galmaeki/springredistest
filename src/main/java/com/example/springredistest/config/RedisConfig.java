f8959c02f934a40d803d3e3f7379a66ec269b3c3
Galmaeki <kjh09800@gmail.com>
2023-03-17 14:12:05 +0900

    feat: 비밀번호 추가

--- src/main/java/com/example/springredistest/config/RedisConfig.java
+++ src/main/java/com/example/springredistest/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -15,9 +16,19 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host,port);
+        //추가된 부분
+        RedisStandaloneConfiguration rds = new RedisStandaloneConfiguration();
+        rds.setHostName(host);
+        rds.setPort(port);
+        rds.setPassword(password);
+        //추가된 부분
+        return new LettuceConnectionFactory(rds);
+//        return new LettuceConnectionFactory(host,port);
     }
 
     @Bean
